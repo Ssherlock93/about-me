f3f35e93c967336ecd7a97b188c91480444443a4
trevorstam <trevorstam@gmail.com>
2018-07-12 14:27:49 -0700

    wrapped functions around questions, fixed break

--- aboutme.js
+++ aboutme.js
@@ -10,85 +10,127 @@ var userName = prompt('Hello, what is your name?');
 console.log('userName', userName);
 
 
-//height question 
-var userHeight = prompt('Hi, ' + userName + '. How tall are you? (in inches)');
-if (userHeight > 72) {
-  alert('That is so tall!');
-}   if(userHeight <= 72){
-  alert('That\'s about average');
-}   else if(userHeight < 60){
-  alert('I am sorry to hear that');
-}
-console.log('userHeight', userHeight);
+//height question .
+//Use this question and wrap it in a function.
+//Function is called heightAnswer()
+//Function input is what user types in box (in order to call the function). This has to be a numeric value.
 
+function questionOne() {
 
-// content with height question
-prompt('Answer the following questions with Yes or No ');
-var userHappy = prompt('Well, ' + userName + ', Are you happy with your height?');
-if (userHappy === 'Yes') {
-  alert('That is great to hear');
-  correct++;
-}   else if (userHappy === 'No') {
-  alert('Well, there isn\'t much you can do about it');
-  incorrect++;
-}   else {
-  alert(wrong);
-}  
-console.log('userHappy', userHappy);
-
-
-
-// car question
-var userCars = prompt(userName + ',Do you like cars?');
-while (userCars !== 'Yes') {   
-  userCars = prompt(wrong);
-  break;
-  correct ++;
-} if (userCars === 'No') {
-  alert('There must be something else that intersts you then');
-  incorrect ++;
-} else {
-  alert(wrong);
-}
-console.log('userCars', userCars);
-
-
-//coding question
-var userCode = prompt('Do you love coding? (Trick question!)');
-if (userCode === 'Yes') {
-  alert ('Great answer');
-  correct ++;
-} else if (userCode === 'No') {
-  alert ('I\'ll accept that, but I don\'t like it');
-  incorrect ++;
-} else {
-  alert(wrong);
+  var userHeight = prompt('Hi, ' + userName + '. How tall are you? (in inches)');
+  if (userHeight > 72) {
+    alert('That is so tall!');
+  }
+  if (userHeight <= 72) {
+    alert('That\'s about average');
+  } else if (userHeight < 60) {
+    alert('I am sorry to hear that');
+  }
+  console.log('userHeight', userHeight);
 }
 
-console.log('userCode', userCode);
+questionOne();
 
-//traffic question
-var userTraffic = prompt('Do you enjoy Seattle traffic?');
-if (userTraffic === 'Yes') {
-  alert ('That\'s strange!');
-  incorrect ++;
-} else if (userTraffic ==='No') {
-  alert('Don\'t we all');
-  correct ++;
-} else {
-  alert (wrong);
+
+
+
+
+
+// // content with height question
+
+function questionTwo() {
+  prompt('Answer the following questions with Yes or No ');
+  var userHappy = prompt('Well, ' + userName + ', Are you happy with your height?');
+  if (userHappy === 'Yes') {
+    alert('That is great to hear');
+    correct++;
+  } else if (userHappy === 'No') {
+    alert('Well, there isn\'t much you can do about it');
+    incorrect++;
+  } else {
+    alert(wrong);
+  }
+  console.log('userHappy', userHappy);
+  
 }
 
-//indent question
-var userEye = prompt('Are your eyes green?');
-if (userEye === 'No') {
-  alert ('correct!');
-  correct ++;
-}   else if (userEye ==='Yes'){
-  alert(wrong);
-  incorrect ++;
-}
-console.log('userTraffic', userTraffic);
+questionTwo();
 
-console.log('correct', correct);
-console.log('incorrect', incorrect);
+
+
+// car questions
+
+function questionThree() {
+  var userCars = prompt(userName + ',Do you like cars?');
+  while (userCars !== 'Yes') {
+    userCars = prompt(wrong);
+    correct++;
+    break;
+
+  }
+  if (userCars === 'No') {
+    alert('There must be something else that intersts you then');
+    incorrect++;
+  } else {
+    alert(wrong);
+  }
+  console.log('userCars', userCars);
+  
+}
+
+questionThree();
+
+
+// //coding question
+function questionFour() {
+  var userCode = prompt('Do you love coding? (Trick question!)');
+  if (userCode === 'Yes') {
+    alert('Great answer');
+    correct++;
+  } else if (userCode === 'No') {
+    alert('I\'ll accept that, but I don\'t like it');
+    incorrect++;
+  } else {
+    alert(wrong);
+    console.log('userCode', userCode);
+  }
+}
+
+questionFour();
+
+
+//;
+
+// //traffic question
+function questionFive() {
+  var userTraffic = prompt('Do you enjoy Seattle traffic?');
+  if (userTraffic === 'Yes') {
+    alert('That\'s strange!');
+    incorrect++;
+  } else if (userTraffic === 'No') {
+    alert('Don\'t we all');
+    correct++;
+  } else {
+    alert(wrong);
+  }
+  console.log('userTraffic', userTraffic);
+}
+
+questionFive();
+
+// //indent question
+function questionSix() {
+  var userEye = prompt('Are your eyes green?');
+  if (userEye === 'No') {
+    alert('correct!');
+    correct++;
+  } else if (userEye === 'Yes') {
+    alert(wrong);
+    incorrect++;
+  }
+  
+
+  console.log('correct', correct);
+  console.log('incorrect', incorrect);
+}
+questionSix();
