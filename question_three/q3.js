853d0d0c0fa727d823ef7257de3248c6ef3df52e
Sean <seansherlock18@gmail.com>
2018-07-15 20:53:31 -0700

    Final question three

--- question_three/q3.js
+++ question_three/q3.js
@@ -1,16 +1,16 @@
 
 function sumAndMultiply(a,b,c) {
-  var sumTotal = sum + c;
-  var productTotal = multiply * c;
+  var sumTemp = sum(a,b);
+  var sumTotal = sum(sumTemp, c);
+  var productTemp = multiply(a,b);
+  var productTotal = multiply(productTemp, c);
   var arr = [];
-  arr.push(sumTotal, productTotal);
-  var message = a + 'and ' + b + 'and ' + c + 'sum to ' + sumTotal + '.';
+  arr.push(sumTotal);
+  arr.push(productTotal);
+  var message = a + 'and ' + b + 'and ' + c + 'sum to ' + sumTotal + '.' + "The product of" + a + 'and' + b + 'and' + c + 'is' + productTotal + '.';
   arr.push(message);
   
 }
-
-
-
 
 function multiply(a,b) {
   var product1 = a * b;
@@ -19,12 +19,10 @@ function multiply(a,b) {
 
 }
 
-
-
-
 function sum(a, b) {
   var sum1 = a + b;
   var arr = [];
   arr.push(sum1);
   
 }
+
